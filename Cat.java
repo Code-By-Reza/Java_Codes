2ac20777aab3df362a4d3f32bb0b5fbe3f893212
Reza <rk.kouhpayeh@.com>
2025-01-17 12:16:51 +0100

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,10 +1,42 @@
 public class Cat {
 
     // public void getInstanceVariable() {
-    //     App.output("Objektadresse: " + this);
+    // App.output("Objektadresse: " + this);
     // }
 
-    public Cat getInstanceVariable(){
+    private String firstName;
+    private String furColor;
+    private int age;
+
+    public Cat(String firstName, String furCooler, int age) {
+        this.firstName = firstName;
+        this.furColor = furCooler;
+        this.age = age;
+    }
+
+
+
+    public Cat getInstanceVariable() {
         return this;
+    }
+
+    /*
+     * Getter
+     */
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+
+    public String getFurColor() {
+        return furColor;
+    }
+
+
+
+    public int getAge() {
+        return age;
     }
 }
