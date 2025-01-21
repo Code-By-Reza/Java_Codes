003ee1cba2e40cdb6e3153d30d9ecae7f77edfd7
Reza <rk.kouhpayeh@.com>
2025-01-21 11:11:48 +0100

    Update App.java file

--- App.java
+++ App.java
@@ -1,4 +1,3 @@
-
 public class App {
 
     public static void main(String[] args) {
@@ -6,26 +5,19 @@ public class App {
         // Instanziierung + Wertzuweisung
         Cat cat = new Cat("Grizabella", "white", 29);
 
-        // output("Blick von aussen: " + cat);
-        // output("Blick von innen: " + cat.getInstanceVariable());
-
         // Werte über Getter
-        output(cat.getFirstName());
-        output(cat.getFurColor());
-        output(Integer.toString(cat.getAge()));
+        output(cat.getStringAttributes("#name"));
+        output(cat.getStringAttributes("#color"));
+        output(cat.getAge());
 
         output("-----------------------");
 
         Cat cat2 = new Cat("Alonzo", "grey", 35);
-        // output("Blick von aussen: " + cat2);
-        // output("Blick von innen: " + cat2.getInstanceVariable());
 
-        cat2.setAge(36); // Setter
-
-        // Werte ausgeben
-        output(cat2.getFirstName());
-        output(cat2.getFurColor());
-        output(Integer.toString(cat2.getAge()));
+        // Werte über Getter
+        output(cat2.getStringAttributes("#name"));
+        output(cat2.getStringAttributes("#color"));
+        output(cat2.getAge());
 
     }
 
